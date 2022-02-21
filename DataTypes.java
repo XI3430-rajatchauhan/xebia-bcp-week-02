8b0802dda04fac35f48acc7002441d4f521aea2b
XI3430-rajatchauhan <99252202+XI3430-rajatchauhan@users.noreply.github.com>
2022-02-21 09:07:41 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,35 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte age=22;
+		short s=56;
+		int n1=2;
+		char ch='R';
+		float f=34.2f;
+		double d=5.65D;
+		boolean b1=true;
+		Long l=874638732L;
+		System.out.println(age);
+		System.out.println(s);
+		System.out.println(n1);
+		System.out.println(ch);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(l);
+		System.out.println(b1);
+		System.out.println("byte min is " + Byte.MIN_VALUE);
+		System.out.println("byte max is " + Byte.MAX_VALUE);
+		System.out.println("short min is " + Short.MIN_VALUE);
+		System.out.println("short max is " + Short.MAX_VALUE);
+		System.out.println("int min is " + Integer.MIN_VALUE);
+		System.out.println("int max is " + Integer.MAX_VALUE);
+		System.out.println("long min is " + Long.MIN_VALUE);
+		System.out.println("long max is " + Long.MAX_VALUE);
+		System.out.println("float min is " + Float.MIN_VALUE);
+		System.out.println("folat max is " + Float.MAX_VALUE);
+		System.out.println("double min is " + Double.MIN_VALUE);
+		System.out.println("double max is " + Double.MAX_VALUE);
+	}
+
+}
